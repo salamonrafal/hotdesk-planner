2de471a8b4059ef72d77590bb834cd529e8066c0
Rafal Salamon <rasa@salamonrafal.pl>
2021-09-11 10:51:21 +0200

    Added Desk Controller structure

---
+++ Api/Controllers/DesksController.cs
@@ -0,0 +1,53 @@
+﻿using Core.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/v1/desks")]
+    [ApiController]
+    public class DesksController : ControllerBase
+    {
+        [HttpGet]
+        public List<Desk> Get ()
+        {
+            return new List<Desk>() { };
+        }
+
+        [HttpGet]
+        [Route("{deskId}")]
+        public Desk GetById([FromRoute] int deskId)
+        {
+            return new Desk() { };
+        }
+
+        [HttpPut]
+        public int Insert([FromBody] Desk desk)
+        {
+            return 0;
+        }
+
+        [HttpPatch]
+        public void Update([FromBody] Desk desk)
+        {
+
+        }
+
+        [HttpDelete]
+        public void Delete([FromBody] Desk desk)
+        {
+
+        }
+
+        [HttpPost]
+        [Route("search")]
+        public List<Desk> Search([FromBody] Desk searcgQuery)
+        {
+            return new List<Desk>(); 
+        }
+    }
+}
