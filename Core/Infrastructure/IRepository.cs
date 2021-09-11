2d7cc34d83c6ebbb92b779fdde1584cf2957cfe5
Rafal Salamon <rasa@salamonrafal.pl>
2021-09-11 08:16:34 +0200

    Added Generic IRepository interface and prepare structure of infrastructure solution

---
+++ Core/Infrastructure/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Infrastructure
+{
+    public interface IRepository<TClass>
+    {
+        public int Insert(TClass data);
+        public TClass Select(TClass data);
+        public void Update(TClass data);
+        public void Delete(TClass data);
+    }
+}
---
+++ Infrastructure/Repositories/DeskRepository.cs
@@ -0,0 +1,27 @@
+﻿using Core.Infrastructure;
+
+namespace Infrastructure.Repositories
+{
+    public class DeskRepository<TClass> : IRepository<TClass>
+    {
+        public void Delete(TClass data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Insert(TClass data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TClass Select(TClass data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(TClass data)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
