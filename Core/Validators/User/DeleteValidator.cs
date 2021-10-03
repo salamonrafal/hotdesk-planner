d2172060a30c43af2e37404ed041aa378e3883fb
Rafal Salamon <rasa@salamonrafal.pl>
2021-10-03 17:45:11 +0200

    Update User Validator

--- Core/Validators/User/DeleteValidator.cs
+++ Core/Validators/User/DeleteValidator.cs
@@ -1,9 +1,16 @@
-﻿using FluentValidation;
+﻿using Core.Enums;
+using FluentValidation;
 
 namespace Core.Validators.User
 {
     public class DeleteValidator: AbstractValidator<Models.User>
     {
-        
+        public DeleteValidator()
+        {
+            RuleSet (nameof(ValidationModelType.Delete), () =>
+            {
+                RuleFor (model => model.Id).NotEmpty ();
+            });
+        }
     }
 }
--- Core/Validators/User/GetOneValidator.cs
+++ Core/Validators/User/GetOneValidator.cs
@@ -1,9 +1,17 @@
-﻿using FluentValidation;
+﻿using Core.Enums;
+using FluentValidation;
 
 namespace Core.Validators.User
 {
     public class GetOneValidator: AbstractValidator<Models.User>
     {
-        
+        public GetOneValidator()
+        {
+            RuleSet (nameof(ValidationModelType.GetOne), () =>
+            {
+                RuleFor (model => model.Id)
+                    .NotEmpty ();
+            });
+        }
     }
 }
